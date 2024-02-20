696d17ce4da7392b561d867e79ce8629bda5157b
lahaina <nattolecats@gmail.com>
2024-02-20 22:20:15 +0900

    monet: Use Style.MONOCHROMATIC by default

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
@@ -156,7 +156,7 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
     private float mContrast = 0;
     // Theme variant: Vibrant, Tonal, Expressive, etc
     @VisibleForTesting
-    protected Style mThemeStyle = Style.TONAL_SPOT;
+    protected Style mThemeStyle = Style.MONOCHROMATIC;
     // Accent colors overlay
     private FabricatedOverlay mSecondaryOverlay;
     // Neutral system colors overlay
@@ -904,11 +904,11 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
                 style = Style.valueOf(
                         object.getString(ThemeOverlayApplier.OVERLAY_CATEGORY_THEME_STYLE));
                 if (!validStyles.contains(style)) {
-                    style = Style.TONAL_SPOT;
+                    style = Style.MONOCHROMATIC;
                 }
             } catch (JSONException | IllegalArgumentException e) {
                 Log.i(TAG, "Failed to parse THEME_CUSTOMIZATION_OVERLAY_PACKAGES.", e);
-                style = Style.TONAL_SPOT;
+                style = Style.MONOCHROMATIC;
             }
         }
         return style;
