98f7b1183b7895de0f19472edcace7ee4f16b5e8
lahaina <nattolecats@gmail.com>
2024-10-08 20:09:50 +0900

    PIHooks: Remove config_certifiedBuildProperties * We are currently using system property for spoofing so this is no needed

--- core/java/com/android/internal/util/PropImitationHooks.java
+++ core/java/com/android/internal/util/PropImitationHooks.java
@@ -83,7 +83,6 @@ public class PropImitationHooks {
         "PIXEL_EXPERIENCE"
     );
 
-    private static volatile String[] sCertifiedProps;
     private static volatile String sStockFp, sNetflixModel;
 
     private static volatile String sProcessName;
@@ -104,7 +103,6 @@ public class PropImitationHooks {
             return;
         }
 
-        sCertifiedProps = res.getStringArray(R.array.config_certifiedBuildProperties);
         sStockFp = res.getString(R.string.config_stockFingerprint);
         sNetflixModel = res.getString(R.string.config_netflixSpoofModel);
 
@@ -184,10 +182,6 @@ public class PropImitationHooks {
     }
 
     private static void setCertifiedPropsForGms() {
-        if (sCertifiedProps.length == 0) {
-            dlog("Certified props are not set");
-            return;
-        }
         final boolean was = isGmsAddAccountActivityOnTop();
         final TaskStackListener taskStackListener = new TaskStackListener() {
             @Override
